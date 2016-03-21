31c1b3f5be71d9ea25b59c0096e89f45466dbedc
Nikhil Malhotra <nikhil.malhotra42@gmail.com>
2016-03-20 22:54:22 -0400

    Question 3 Part 2

--- Lab_2_2.m
+++ Lab_2_2.m
@@ -15,10 +15,52 @@ sal = cov(al);
 sbl = cov(bl);
 scl = cov(cl);
 
+sizeal = size(al);
+sizebl = size(bl);
+sizecl = size(cl);
 
+yal = mvnpdf(al,ual,sal);
+ybl = mvnpdf(bl,ubl,sbl);
+ycl = mvnpdf(cl,ucl,scl);
+
+xa = [];
+ya = [];
+xb = [];
+yb = [];
+xc = [];
+yc = [];
+x = [];
+y = [];
+
+for a = 0:1:450
+    for b = 0:1:450
+        yal = mvnpdf([a,b],ual,sal);
+        ybl = mvnpdf([a,b],ubl,sbl);
+        ycl = mvnpdf([a,b],ucl,scl);
+        if ( yal > ybl && yal > ycl )
+            xa(end+1) = a;
+            ya(end+1) = b;
+        elseif ( ybl > yal && ybl > ycl )
+            xb(end+1) = a;
+            yb(end+1) = b;
+        elseif ( ycl > yal && ycl > ybl )
+            xc(end+1) = a;
+            yc(end+1) = b;
+        else
+            x(end+1) = a;
+            y(end+1) = b;
+        end
+    end 
+end
 
 hold on;
+plot(xa,ya,xb,yb,xc,yc,x,y);
 scatter(al(:,1),al(:,2))
 scatter(bl(:,1),bl(:,2))
 scatter(cl(:,1),cl(:,2))
+title('Parametric Estimation');
+xlabel('X');
+ylabel('Y');
+legend('Boundary AL','Boundary BL','Boundary CL','AL','BL','CL');
 hold off;
+
