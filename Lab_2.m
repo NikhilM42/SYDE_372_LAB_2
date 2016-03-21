863c5d922d38a8f969224fbee27b44e97b65affa
migdea11 <migdea11@gmail.com>
2016-03-20 20:25:48 -0400

    Uniform distribution
    Finished the uniform distribution question

--- Lab_2.m
+++ Lab_2.m
@@ -20,7 +20,7 @@ sizeb = size(b);
 %variance
     %set a
     sa = cov(a);
-
+    %% Part 2.1
 figure(1);
 Xa = min(a):((max(a)-min(a))/sizea(2)):max(a);
 Ya = normpdf(Xa,ua,sa);
@@ -31,6 +31,7 @@ title('Parametric Estimation - Gaussian');
 legend('Estimate','Real','Estimate Mean','Real Mean');
 hold off;
 
+    %% part 2.2
 rlambda = 1/((1/sizeb(2))*sum(b,2));
 figure(2);
 Xb = min(b):((max(b)-min(b))/sizeb(2)):max(b);
@@ -39,5 +40,31 @@ Ybr = exppdf(Xb,rlambda);
 hold on;
 plot(Xb,Yb,Xb,Ybr,[lambda ; lambda],[0 ; 0.5],[rlambda ; rlambda],[0 ; 0.5]);
 title('Parametric Estimation - Exponential');
+legend('Estimate','Real','Estimate Mean','Real Mean');
+hold off;
+
+    %% Part 2.3a
+ 
+figure(3);
+uc_a = (max(a)+min(a))/2;
+Xc_a = min(a):((max(a)-min(a))/sizea(2)):max(a);
+Yc_a = unifpdf(Xc_a,min(a),max(a));
+Yc_ar = normpdf(Xc_a,rua,rsa);
+hold on;
+plot(Xc_a,Yc_a,Xc_a,Yc_ar,[uc_a ; uc_a],[0 ; 0.5],[rua ; rua],[0 ; 0.5]);
+title('Parametric Estimation - Uniform (Part A)');
+legend('Estimate','Real','Estimate Mean','Real Mean');
+hold off;
+
+    %% Part 2.3b
+ 
+figure(4);
+uc_b = (max(b)+min(b))/2;
+Xc_b = min(b):((max(b)-min(b))/sizea(2)):max(b);
+Yc_b = unifpdf(Xc_b,min(b),max(b));
+Yc_br = exppdf(Xb,rlambda);
+hold on;
+plot(Xc_b,Yc_b,Xc_b,Yc_br,[uc_b ; uc_b],[0 ; 0.5],[rlambda ; rlambda],[0 ; 0.5]);
+title('Parametric Estimation - Uniform (Part B)');
 legend('Estimate','Real','Estimate Mean','Real Mean');
 hold off;
