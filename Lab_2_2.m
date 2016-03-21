2f32ec86075ce78f35ea9bd7451c95b94751b89f
Nikhil Malhotra <nikhil.malhotra42@gmail.com>
2016-03-20 20:46:04 -0400

    Folder for Figures

--- Lab_2_2.m
+++ Lab_2_2.m
@@ -5,3 +5,20 @@ load('lab2_2.mat','al');
 load('lab2_2.mat','bl');
 load('lab2_2.mat','cl');
 
+%% Maximum likelihood method
+%% Parametric Estimation - Gaussian
+
+ual = mean(al);
+ubl = mean(bl);
+ucl = mean(cl);
+sal = cov(al);
+sbl = cov(bl);
+scl = cov(cl);
+
+
+
+hold on;
+scatter(al(:,1),al(:,2))
+scatter(bl(:,1),bl(:,2))
+scatter(cl(:,1),cl(:,2))
+hold off;
